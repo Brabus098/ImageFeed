c5b87f943e603b853f1fa21f5a98b7549d2dce25
Brabus098 <vovan1998888@mail.ru>
2024-08-01 10:00:00 +0300

    Задание sprint_08

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,15 @@
+//  ImagesListCell.swift
+
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var cellImageView: UIImageView!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    
+    
+}
---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,79 @@
+//  ViewController.swift
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    private let photosName: [String] = Array(0..<20).map { "\($0)"}
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        formatter.dateFormat = "d MMMM yyyy"
+        formatter.locale = Locale(identifier: "ru_RU")
+        return formatter
+    }()
+    var todayDate = Date()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0) // Добавляем отступы сверху и снизу для контента таблицы
+    }
+    
+    // Метод настройки строки
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        cell.likeButton.setTitle("", for: .normal)
+        indexPath.row % 2 == 0 ? cell.likeButton.setImage(.unactiveLike, for: .normal) : cell.likeButton.setImage(.activeLike, for: .normal)
+        
+        // Проверяем есть ли значение по адресу
+        guard let newImage = UIImage(named: "\(indexPath.row)") else {return}
+        cell.cellImageView.image = newImage // задаем картинку
+        cell.dateLabel.text = dateFormatter.string(from: todayDate) // задаем дату
+
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    // Метод вызывается когда пользователь нажимает на ячейку
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    // Динамический расчет высоты строки
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+        
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource{
+
+    // Метод определяет количество строк в секции таблицы
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photosName.count
+    }
+    
+    // Метод создает и настраивает ячейку для конкретной строки
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath) // 1 получаем ячейку из пула переиспользуемых ячеек
+        
+        guard let imageListCell = cell as? ImagesListCell else { // 2 Проверяем что она нужного типа
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell, with: indexPath) // 3 Вызываем метод настройки
+        
+        return imageListCell // 4 Возвращаем получившуюся строку
+    }
+        
+}
